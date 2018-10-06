94f03305983350625f41118b2a14917e5d7a36c6
SenpaiEnder <37985108+SenpaiEnder@users.noreply.github.com>
2018-10-05 23:01:59 -0400

    Update rules.js

--- app/commands/rules.js
+++ app/commands/rules.js
@@ -7,7 +7,7 @@ module.exports = {
         const embed = new Discord.RichEmbed()
         .setColor("42C0FB")
         .addField("Please read #rules for now!")
-		.setFooter('© Aevum Studios', 'https://media.discordapp.net/attachments/425141295648866304/497827096211750922/image0.jpg?width=270&height=270')
+	.setFooter('© Aevum Studios', 'https://media.discordapp.net/attachments/425141295648866304/497827096211750922/image0.jpg?width=270&height=270')
         .setTimestamp(message.timestamp)
         
         message.channel.send(embed);
