87743ce01be9ea48fa2d09e9bfe6c2a1f12a1cab
SenpaiEnder <37985108+SenpaiEnder@users.noreply.github.com>
2018-10-05 23:01:02 -0400

    Update help.js

--- app/commands/help.js
+++ app/commands/help.js
@@ -8,8 +8,7 @@ module.exports = {
         .setColor("42C0FB")
         .setTitle("**Command Guide**")
         .addField(" `-new` - Creates a ticket!\n `-rules` - Displays the current rules  of the server!\n `-terms` - Displays the terms of service!\n `-add [@name] [#yourticket]` - Adds member to your ticket!")
-        .setFooter('© Aevum Studios', 'https://media.discordapp.net/attachments/425141295648866304/497827096211750922/image0.jpg?width=270&height=270
-')
+        .setFooter('© Aevum Studios', 'https://media.discordapp.net/attachments/425141295648866304/497827096211750922/image0.jpg?width=270&height=270')
         .setTimestamp(message.timestamp)
         
         
