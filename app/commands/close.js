be2cc824b9bccb72013a452c8c96b2d809313c77
SenpaiEnder <37985108+SenpaiEnder@users.noreply.github.com>
2018-10-05 23:00:35 -0400

    Update close.js

--- app/commands/close.js
+++ app/commands/close.js
@@ -9,8 +9,7 @@ module.exports = {
             .setColor("ffcc00")
             .setTitle("Error! ❌")
             .setDescription("You cannot execute this command outside of a ticket channel!")
-            .setFooter('© Aevum Studios', 'https://media.discordapp.net/attachments/425141295648866304/497827096211750922/image0.jpg?width=270&height=270
-')
+            .setFooter('© Aevum Studios', 'https://media.discordapp.net/attachments/425141295648866304/497827096211750922/image0.jpg?width=270&height=270')
             .setTimestamp(message.timestamp)
         
             return message.channel.send(errorEmbed);
@@ -21,8 +20,7 @@ if(!message.member.hasPermission("MANAGE_MESSAGES")){
     .setColor("ffcc00")
     .setTitle("Error! ❌")
     .setDescription("You do not have permission to close this ticket!")
-    .setFooter('© Aevum Studios', 'https://media.discordapp.net/attachments/425141295648866304/497827096211750922/image0.jpg?width=270&height=270
-')
+    .setFooter('© Aevum Studios', 'https://media.discordapp.net/attachments/425141295648866304/497827096211750922/image0.jpg?width=270&height=270')
     .setTimestamp(message.timestamp)
         
     return message.channel.send(errorEmbed);          
@@ -32,8 +30,7 @@ const warnEmbed = new Discord.RichEmbed()
 .setColor("ff2600")
 .setTitle("Warning! ⚠️")
 .setDescription("Are you absolutely sure you want to delete this ticket? To confirm and proceed with this action, please type `-confirm`.")
-.setFooter('© Aevum Studios', 'https://media.discordapp.net/attachments/425141295648866304/497827096211750922/image0.jpg?width=270&height=270
-')
+.setFooter('© Aevum Studios', 'https://media.discordapp.net/attachments/425141295648866304/497827096211750922/image0.jpg?width=270&height=270')
 .setTimestamp(message.timestamp)
         
 message.channel.send(warnEmbed)   
